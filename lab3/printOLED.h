e305d706c16af19c25880e738a45a09822d8f53e
Elias Mapendo <127340799+emapendo@users.noreply.github.com>
2025-02-19 13:39:41 -0800

    Update printOLED.h

--- lab3/printOLED.h
+++ lab3/printOLED.h
@@ -1,17 +1,29 @@
-#ifndef PrintOLED_h
-#define PrintOLED_h
+#ifndef Odometry_h
+#define Odometry_h
 #include <Pololu3piPlus32U4.h>
 using namespace Pololu3piPlus32U4;
 
-class PrintOLED{
+class Odometry{
   public:
-    PrintOLED();
-    void print_encoder(float L, float R);
-    void print_float(float x);
-    void print_odom(float x, float y, float phi);
+    Odometry::Odometry(float diaL, float diaR, float w, int nL, int nR, int gearRatio, bool dead_reckoning);
+    void Odometry::update_odom(int left_encoder_counts, int right_encoder_counts, float &x, float &y, float &theta);
     
   private:
-    int _lastUpdateTime;
+    float _diaL;
+    float _diaR;
+    float _w;
+    int _nL;
+    int _nR;
+    int _gearRatio;
+    IMU _imu;
+    float _IMUavg_error;
+    bool _deadreckoning;
+    int _left_encoder_counts_prev;
+    int _right_encoder_counts_prev;
+
+    double _x;
+    double _y;
+    double _theta;
 };
 
 #endif
