b06778de7d8a1e16f4f5471ddba9e6888cf7409d
joshuaconstine <joshua.constine97@gmail.com>
2025-05-01 16:50:19 -0700

    adjusted turn delay

--- final/main/main.ino
+++ final/main/main.ino
@@ -27,7 +27,7 @@ bool isOnBlack = false;
 #define CELEBRATION_DURATION 3200
 #define SPIN_SPEED 100
 #define MOVE_SPEED 100
-#define TURN_DELAY 900
+#define TURN_DELAY 850
 #define MOVE_DELAY 2200
 
 // Maze dimensions: 80x180 cm → 8x18 grid (10 cm cells)
